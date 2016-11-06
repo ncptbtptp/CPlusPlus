0b9c43014ea6e013c1a5204523c87fbb5b299a55
ncptbtptp <yingmu1983@gmail.com>
2016-11-06 22:04:09 +0800

    Update shared memory example

--- linux/shm.cpp
+++ linux/shm.cpp
@@ -1,8 +1,12 @@
 #include <sys/mman.h>   // shm_open
+#include <sys/wait.h>   // waitpid
 #include <fcntl.h>      // open flags(O_CREAT..)
 #include <stdio.h>      // perror
 #include <unistd.h>     // ftruncate
+#include <ctime>
+#include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 void error_out(const string &msg)
@@ -25,13 +29,62 @@ int main()
     }
 
     // Resize the size of an open file
-    int r = ftruncate(fd, share_size);
-    if (r != 0) {
+    int res = ftruncate(fd, share_size);
+    if (res != 0) {
         error_out("ftruncate");
     }
 
-   void *ptr = mmap(0, share_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); 
+   void *ptr = mmap(0, share_size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+   if (ptr == MAP_FAILED) {
+       error_out("mmap");
+   }
+   int *data = static_cast<int*>(ptr);
 
-    return 0;
+   // Close the file after mmap() doesn't affect the mapping
+   close(fd);
 
+   pid_t pid = fork();
+   if (pid == 0) {
+       // Child process prepares the data to be sorted in parent process.
+       // Data format: # of data(int), followed by # integers to sort.
+       cout << "[Child] PID: " << getpid() << endl;
+
+       int sort_num = 10;
+       *data = sort_num;
+       srand(time(NULL));
+       for (size_t i = 0; i < sort_num; ++i) {
+           data[i + 1] = rand() % sort_num;
+       }
+
+       cout << "[Child] Data:\n";
+       for_each(data, data + sort_num + 1, [](int i) { cout << i << ", "; });
+       cout << endl;
+
+       // Child process terminates
+       exit(0);
+   }
+   else {
+       cout << "[Parent] PID: " << getpid() << endl;
+
+       int status;
+       waitpid(pid, &status, 0);
+       vector<int> v(data, data + data[0] + 1);
+
+       sort(v.begin() + 1, v.end());
+       cout << "[Parent] Data:\n";
+       for_each(v.cbegin(), v.cend(), [](int i) { cout << i << ", "; });
+       cout << endl;
+   }
+
+   res = munmap(ptr, share_size);
+   if (res != 0) {
+       error_out("munmap");
+   }
+
+   res = shm_unlink(share_name.c_str());
+   if (res != 0) {
+       error_out("shm_unlink");
+   }
+
+   return 0;
 }
